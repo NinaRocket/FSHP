0d60e34b937b173da5e476a1625bc77bdc826afe
NRocket <ninamrocket@gmail.com>
2020-09-22 07:07:05 -0700

    initialized handelbars

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //Dependencies
 const express = require("express");
-const exhbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 //instance of express app
 const app = express();
@@ -11,6 +11,10 @@ const PORT = process.env.PORT || 8080;
 //set handlebars as default templating engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
 
 //Start server
 app.listen(PORT, function () {
