44cfdc85dc7e278993e4dc949562cdca046a5f3a
NRocket <ninamrocket@gmail.com>
2020-10-04 18:09:01 -0700

    routes added for CRUD functions

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 //Dependencies
 const express = require("express");
 const exphbs = require("express-handlebars");
-const monk = require("monk");
+const db = require("monk")("localhost:27017/fshp");
+const userData = monk.get("user-data");
 
 //instance of express app
 const app = express();
@@ -19,9 +20,44 @@ app.get("/", function (req, res) {
   res.render("index");
 });
 
+//get data
+app.get("/get-data", function (req, res) {
+  const data = userData.find({});
+  data.on("success", function (docs) {
+    res.render("index", { items: docs });
+  });
+});
+
+app.post("/insert", function (req, res, next) {
+  const item = {
+    title: req.body.title,
+    content: req.body.content,
+  };
+
+  userData.insert(item);
+  res.redirect("/");
+});
+
+app.post("/update", function (req, res, next) {
+  const item = {
+    title: req.body.title,
+    content: req.body.content,
+  };
+  const id = req.body.id;
+  //userData.update({ _id: db.id(id) }, item);
+  userData.updateById(id, item);
+});
+
+app.post("/delete", function (req, res, next) {
+  const id = req.body.id;
+  //userData.delete({"_id": db.id(id)});
+  userData.removeById(id);
+});
+
 app.get("/about", function (req, res) {
   res.render("about");
 });
+
 //Start server
 app.listen(PORT, function () {
   console.log("Server listening on: http://localhost:" + PORT);
